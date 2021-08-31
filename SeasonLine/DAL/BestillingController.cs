c612c17ba0a9c6d3239f0bcc9ca86b90cd468a0f
MinhTT43 <72129577+MinhTT43@users.noreply.github.com>
2021-08-31 21:18:00 +0200

    First commit, created database.

--- SeasonLine/DAL/BestillingController.cs
+++ SeasonLine/DAL/BestillingController.cs
@@ -16,6 +16,34 @@ namespace SeasonLine.DAL
             _db = db;
         }
 
+        // TODO: Refaktoriser koden til en egen klasse
+        public Kunder LagKunde(Bestilling InnBestilling)
+        {
+            var nyKunde = new Kunder
+            {
+                Fornavn = InnBestilling.Fornavn,
+                Etternavn = InnBestilling.Etternavn,
+                Epost = InnBestilling.Epost,
+                Telefon = InnBestilling.Telefon,
+            };
+            return nyKunde;
+        }
+
+        // TODO: Refaktoriser koden til en egen klasse
+        public Bestillinger LagBestilling(Bestilling InnBestilling)
+        {
+            var NyBestilling = new Bestillinger
+            {
+                AntallBarn = InnBestilling.AntallBarn,
+                AntallVokse = InnBestilling.AntallVokse,
+                AntallLugarer = InnBestilling.AntallLugarer
+            };
+
+            return NyBestilling;
+        }
+
+
+
         public List<Bestilling> HentAlle()
         {
             List<Kunder> KundeList = _db.AlleKunder.ToList();
